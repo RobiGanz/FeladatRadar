31411259205d8c3db5c82e1da27d057777458ccb
letuana <le.tuan.anh@ganziskola.hu>
2026-03-13 11:54:17 +0100

    themeHelper dark mode mentés

---
+++ FeladatRadar.frontend/wwwroot/theme.js
@@ -0,0 +1,11 @@
+﻿
+//— dark mode téma mentés
+
+window.themeHelper = {
+    getTheme: function () {
+        return localStorage.getItem('fr-theme') || 'light';
+    },
+    setTheme: function (theme) {
+        localStorage.setItem('fr-theme', theme);
+    }
+};
